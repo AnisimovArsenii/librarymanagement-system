67c8a02f503e82ee418ea1bcde84eb05368a2391
AnisimovArsenii <senya2007msk@gmail.com>
2026-01-11 16:15:40 +0300

    Add demo scenario with sample books

--- src/main/java/library/Main.java
+++ src/main/java/library/Main.java
@@ -1,18 +1,40 @@
-package library;
-
 public class Main {
+
     public static void main(String[] args) {
-        // Создание библиотеки
+
         Library library = new Library();
 
-        // TODO: Добавить демонстрационные книги
-        
-        // TODO: Показать работу всех методов библиотеки
-        // - Добавление книг
-        // - Поиск по ID и автору
-        // - Выдача и возврат книг
-        // - Вывод журнала операций
-        
-        System.out.println("Библиотека создана. Добавьте книги для демонстрации.");
+        // Добавление книг
+        library.addBook(new Book(1, "Война и мир",
+                "Л.Н. Толстой", 1869, "978-5-17-090335-2"));
+
+        library.addBook(new Book(2, "Преступление и наказание",
+                "Ф.М. Достоевский", 1866, "978-5-17-090336-9"));
+
+        library.addBook(new Book(3, "Анна Каренина",
+                "Л.Н. Толстой", 1877, "978-5-17-090337-6"));
+
+        // Поиск книги
+        System.out.println(library.findBookById(1));
+
+        // Поиск по автору
+        System.out.println("\nКниги Л.Н. Толстого:");
+        for (Book book : library.findBooksByAuthor("Л.Н. Толстой")) {
+            System.out.println(book);
+        }
+
+        // Выдача и возврат
+        library.borrowBook(1);
+        library.returnBook(1);
+
+        // Доступные книги
+        System.out.println("\nДоступные книги:");
+        for (Book book : library.getAvailableBooks()) {
+            System.out.println(book);
+        }
+
+        // Журнал операций
+        System.out.println();
+        library.printOperationLog();
     }
 }
